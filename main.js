d8a05f75e04dfea09aa7a602621d191f888a0d6a
Elaine Sajets <elainesajets@gmail.com>
2025-01-23 15:18:03 +0900

    Added randomizer, computerChoice and humanChoice functions. randomizer() gets a random number, getComputerChoice() gets the computer's hand after a call to the randomizer() function. getHumanChoice() allows the user to input their hand and checks if the input is valid.

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
 console.log('Hello World');
 
-let computerHand = '';
-let humanHand = '';
-
+let humanScore = 0;
+let computerScore = 0;
 //Create a function named getComputerChoice. This represents the computer.
+
+//Create random number
+function randomize() {
+  let randomNumber = Math.random() * 3; // scales to [0, 3]
+  return Math.floor(randomNumber);
+}
 //The code should return one of the following strings: "rock", "paper", or "scissor".
 function getComputerChoice() {
-  let computerHand = Math.random();
-  let handShow = '';
+  let num = randomize();
+  let computerHand = '';
+  console.log(num);
 
-  /*   if (computerHand > 0 && computerHand < 0.3334) {
-    handShow = 'Rock';
-    return computerHand;
-  } else if (computerHand > 0.3334 && computerHand < 0.6667) {
-    computerHand = 'Paper';
-    return computerHand;
-  } else {
-    computerHand = 'Scissor';
-    return computerHand;
-  } */
-
-  switch (computerHand) {
-    case computerHand > 0 && computerHand < 0.3334:
-      handShow = 'Rock';
+  switch (num) {
+    case 0:
+      computerHand = 'rock';
       break;
-    case computerHand > 0.3334 && computerHand < 0.6667:
-      handShow = 'Paper';
+    case 1:
+      computerHand = 'paper';
       break;
-    default:
-      handShow = 'Scissor';
+    case 2:
+      computerHand = 'scissor';
       break;
   }
 
-  console.log(`Computer shows: ${handShow}`);
+  return computerHand;
+  console.log(`Computer shows: ${computerHand}`);
+}
+
+// User will select their choice
+function getHumanChoice() {
+  let choice = prompt('What do you chose?', 'Rock, Paper, or Scissor?');
+  let hand = '';
+
+  if (
+    choice.toLowerCase() === 'scissor' ||
+    choice.toLowerCase() === 'rock' ||
+    choice.toLowerCase() === 'paper'
+  ) {
+    hand = choice;
+    return hand.toLowerCase();
+  } else {
+    alert('Invalid input. Try again!');
+    return getHumanChoice();
+  }
+}
+
+function playRound() {
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+
+  console.log(humanChoice);
+  console.log(computerChoice);
 }

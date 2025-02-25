3191a2f4b786885cb039fb010068b967a1a316c8
Elaine Sajets <elainesajets@gmail.com>
2025-02-25 18:29:47 +0900

    Refactored code to update DOM instead of using console.log. Also added a font I didn't end up using and an audio to play when you win.

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const gameContainer = document.getElementById('game-container');
+const gameResult = document.getElementById('game-result');
+const gameScore = document.getElementById('game-score');
+const roundNumber = document.getElementById('round-number');
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
 //Create random number
 function randomize() {
   let randomNumber = Math.random() * 3; // scales to [0, 3]
@@ -36,65 +45,118 @@ function getHumanChoice() {
   }
 }
 
-// Start 5 rounds of Rock Paper Scissor
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-  const maxRounds = 5;
-  let round = 0;
+function newGame() {
+  const newGame = document.createElement('button');
+  newGame.textContent = 'Try again';
+  gameScore.appendChild(newGame);
 
-  // Finish the game after 5 rounds
-  while (round < maxRounds) {
-    // Start a round of the game and check the winner
-    function playRound(human, computer) {
-      let result = '';
-      switch (true) {
-        case human === computer:
-          result = "It's a draw";
-          break;
-        case human === 'scissor' && computer === 'paper':
-          result = 'Scissor beats paper. You win! :)';
-          humanScore += 1;
-          break;
-        case human === 'rock' && computer === 'scissor':
-          result = 'Rock beats scissor. You win! :)';
-          humanScore += 1;
-          break;
-        case human === 'paper' && computer === 'rock':
-          result = 'Paper beats rock. You win! :)';
-          humanScore += 1;
-          break;
-        default:
-          result = `${computer} beats ${human}. You lose! :(`;
-          computerScore += 1;
-          break;
-      }
+  newGame.addEventListener('click', resetGame);
+}
 
-      //Increment the round number.
-      round += 1;
+const win = new Audio('sound/mixkit-good.wav');
 
-      //Print the results to console
-      console.log('=================================');
-      console.info(`%c Round: ${round}`, `font-weight: bold;`);
-      console.log(`Result: ${result}`);
-      console.log(`Your score: ${humanScore}`);
-      console.log(`Computer's score: ${computerScore}`);
-    }
+function playRound(human, computer) {
+  const result = document.createElement('p');
+  const yourScore = document.createElement('p');
+  const compScore = document.createElement('p');
 
-    //Assigns the human choice and computer choice to arguments that will be passed to the game
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+  gameResult.textContent = '';
+  gameScore.textContent = '';
 
-    // Starts the round after checking if the human input is valid
-    if (humanSelection === null) {
-      console.log('Player has cancelled the game.');
-      return null;
+  if (humanScore === 5 || computerScore === 5) {
+    if (humanScore > computerScore) {
+      win.play();
+      gameResult.textContent = "You've won the game!";
+      //resetButton.textContent = 'New Game';
+      newGame();
+      return;
     } else {
-      playRound(humanSelection, computerSelection);
+      gameResult.textContent = "You've unfortunately lost. Try again?";
+      newGame();
+      return;
     }
   }
 
-  //Announce the winner of the game based on the scores. As the game is supposed to be maximum 5 rounds, a draw is possible.
+  if (resetButton.textContent === 'New Game') {
+    resetButton.textContent = 'Reset Game';
+  }
+
+  function updateScore(num1, num2) {
+    yourScore.textContent = `Your score: ${num1}`;
+    compScore.textContent = `Computer score: ${num2}`;
+  }
+
+  switch (true) {
+    case human === computer:
+      result.textContent = `Both have selected ${human}! It's a draw.`;
+      updateScore(humanScore, computerScore);
+      break;
+    case human === 'scissor' && computer === 'paper':
+      humanScore += 1;
+      result.textContent = 'Scissor beats paper. You win! :)';
+      updateScore(humanScore, computerScore);
+      break;
+    case human === 'rock' && computer === 'scissor':
+      humanScore += 1;
+      result.textContent = 'Rock beats scissor. You win! :)';
+      updateScore(humanScore, computerScore);
+      break;
+    case human === 'paper' && computer === 'rock':
+      humanScore += 1;
+      result.textContent = 'Paper beats rock. You win! :)';
+      updateScore(humanScore, computerScore);
+      break;
+    default:
+      computerScore += 1;
+      result.textContent = `${computer} beats ${human}. You lose! :(`;
+      updateScore(humanScore, computerScore);
+      break;
+  }
+
+  gameResult.appendChild(result);
+  gameScore.appendChild(yourScore);
+  gameScore.appendChild(compScore);
+
+  round++;
+}
+
+const btns = document.getElementById('game-btns');
+
+btns.addEventListener('click', (e) => {
+  let text = e.target.textContent.toLowerCase().trim();
+  if (text === 'rock' || text === 'paper' || text === 'scissor') {
+    console.log(`You have selected ${text}`);
+    playRound(text, getComputerChoice());
+  }
+});
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  round = 0;
+  console.clear();
+  gameResult.textContent = '';
+  gameScore.textContent = '';
+}
+
+const resetButton = document.getElementById('reset');
+
+resetButton.addEventListener('mouseup', resetGame);
+
+/*   //Assigns the human choice and computer choice to arguments that will be passed to the game
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  // Starts the round after checking if the human input is valid
+  if (humanSelection === null) {
+    console.log('Player has cancelled the game.');
+    return null;
+  } else {
+    playRound(humanSelection, computerSelection);
+  }
+} */
+
+/*   //Announce the winner of the game based on the scores. As the game is supposed to be maximum 5 rounds, a draw is possible.
   if (humanScore > computerScore) {
     console.log('%c You won the game! :D', `color: green;`);
   } else if (humanScore === computerScore) {
@@ -104,11 +166,7 @@ function playGame() {
   }
 
   //Reset the variables, so you can start the game again.
-  function reset() {
-    humanScore = 0;
-    computerScore = 0;
-    round = 0;
-  }
+ 
 
   //Checks if player wants to start the game again.
   if (confirm('Do you want to play again?')) {
@@ -118,4 +176,10 @@ function playGame() {
     console.log('Thank you for playing!');
     return;
   }
-}
+} */
+
+/* const playAgain = document.getElementById('play-again');
+
+playAgain.addEventListener('mouseup', () => {
+  
+}) */

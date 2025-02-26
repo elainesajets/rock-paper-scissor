cc40dc9504b0af378da1277517d6662397c87310
Elaine Sajets <elainesajets@gmail.com>
2025-02-26 17:17:21 +0900

    Big update Added new font, icons and sounds. Changed from switch statement to if else for cleaner code. Removed code leftover from console game project Added sound on all button clicks, even those added by DOM.

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ My submission for the Rock Paper Scissors project from The Odin Project.
 The game is no longer consol based, but has now a UI.
 
 [Project instructions can be found here](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors) [ and here.](https://www.theodinproject.com/lessons/foundations-revisiting-rock-paper-scissors)
+
+Icons from (streamline)[https://www.streamlinehq.com/icons/pixel?icon=ico_vjZACulAoCPnOQIr]
+Sound effects from (mixkit)[https://mixkit.co/free-sound-effects/game/]
--- main.js
+++ main.js
@@ -7,13 +7,15 @@ let humanScore = 0;
 let computerScore = 0;
 let round = 0;
 
-//Create random number
+//Sounds from https://mixkit.co/free-sound-effects/game/
+const win = new Audio('sound/mixkit-good.wav');
+const effect = new Audio('sound/mixkit-game-ball-tap-2073.wav');
+
 function randomize() {
   let randomNumber = Math.random() * 3; // scales to [0, 3]
   return Math.floor(randomNumber);
 }
 
-//Create a function named getComputerChoice. This represents the computer.
 function getComputerChoice() {
   let num = randomize();
 
@@ -22,164 +24,92 @@ function getComputerChoice() {
   } else if (num === 1) {
     return 'paper';
   } else {
-    return 'scissor';
+    return 'scissors';
   }
 }
 
-// User will select their choice
-function getHumanChoice() {
-  let choice = prompt('What do you chose? (Rock, Paper, or Scissor)');
-
-  // Check if the user input is valid
-  if (choice === null) {
-    alert('You have cancelled the game.');
-    return null;
-  }
-  choice = choice.trim().toLowerCase();
-
-  if (choice === 'scissor' || choice === 'rock' || choice === 'paper') {
-    return choice;
-  } else {
-    alert('Invalid input. Try again!');
-    return getHumanChoice();
-  }
+function updateScore() {
+  gameScore.textContent = `Your score: ${humanScore} \n Computer score: ${computerScore}`;
 }
 
-function newGame() {
-  const newGame = document.createElement('button');
-  newGame.textContent = 'Try again';
-  gameScore.appendChild(newGame);
-
-  newGame.addEventListener('click', resetGame);
-}
-
-const win = new Audio('sound/mixkit-good.wav');
-
-function playRound(human, computer) {
-  const result = document.createElement('p');
-  const yourScore = document.createElement('p');
-  const compScore = document.createElement('p');
-
-  gameResult.textContent = '';
-  gameScore.textContent = '';
-
-  if (humanScore === 5 || computerScore === 5) {
-    if (humanScore > computerScore) {
-      win.play();
-      gameResult.textContent = "You've won the game!";
-      //resetButton.textContent = 'New Game';
-      newGame();
-      return;
-    } else {
-      gameResult.textContent = "You've unfortunately lost. Try again?";
-      newGame();
-      return;
-    }
-  }
-
-  if (resetButton.textContent === 'New Game') {
-    resetButton.textContent = 'Reset Game';
-  }
-
-  function updateScore(num1, num2) {
-    yourScore.textContent = `Your score: ${num1}`;
-    compScore.textContent = `Computer score: ${num2}`;
-  }
-
-  switch (true) {
-    case human === computer:
-      result.textContent = `Both have selected ${human}! It's a draw.`;
-      updateScore(humanScore, computerScore);
-      break;
-    case human === 'scissor' && computer === 'paper':
-      humanScore += 1;
-      result.textContent = 'Scissor beats paper. You win! :)';
-      updateScore(humanScore, computerScore);
-      break;
-    case human === 'rock' && computer === 'scissor':
-      humanScore += 1;
-      result.textContent = 'Rock beats scissor. You win! :)';
-      updateScore(humanScore, computerScore);
-      break;
-    case human === 'paper' && computer === 'rock':
-      humanScore += 1;
-      result.textContent = 'Paper beats rock. You win! :)';
-      updateScore(humanScore, computerScore);
-      break;
-    default:
-      computerScore += 1;
-      result.textContent = `${computer} beats ${human}. You lose! :(`;
-      updateScore(humanScore, computerScore);
-      break;
-  }
-
-  gameResult.appendChild(result);
-  gameScore.appendChild(yourScore);
-  gameScore.appendChild(compScore);
-
-  round++;
-}
-
-const btns = document.getElementById('game-btns');
-
-btns.addEventListener('click', (e) => {
-  let text = e.target.textContent.toLowerCase().trim();
-  if (text === 'rock' || text === 'paper' || text === 'scissor') {
-    console.log(`You have selected ${text}`);
-    playRound(text, getComputerChoice());
-  }
-});
-
+// Separate function in case I want to add a reset button to use during the game
 function resetGame() {
   humanScore = 0;
   computerScore = 0;
   round = 0;
   console.clear();
-  gameResult.textContent = '';
+  gameResult.textContent = 'Start the game!';
   gameScore.textContent = '';
+  roundNumber.textContent = '';
+  document.getElementById('game-btns').style.pointerEvents = 'auto'; // Enables if disabled
 }
 
-const resetButton = document.getElementById('reset');
+function newGame() {
+  const newGameButton = document.createElement('button');
+  newGameButton.textContent = 'Try again';
+  roundNumber.appendChild(newGameButton);
 
-resetButton.addEventListener('mouseup', resetGame);
+  newGameButton.addEventListener('click', () => {
+    resetGame();
+    roundNumber.removeChild(newGameButton);
+  });
+}
 
-/*   //Assigns the human choice and computer choice to arguments that will be passed to the game
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
+function playRound(human, computer) {
+  const result = document.createElement('p');
 
-  // Starts the round after checking if the human input is valid
-  if (humanSelection === null) {
-    console.log('Player has cancelled the game.');
-    return null;
+  //Reset textContent of elements
+  roundNumber.textContent = '';
+  gameResult.textContent = '';
+  gameScore.textContent = '';
+
+  if (
+    (human === 'scissors' && computer === 'paper') ||
+    (human === 'rock' && computer === 'scissors') ||
+    (human === 'paper' && computer === 'rock')
+  ) {
+    humanScore += 1;
+    result.textContent = `${human} beats ${computer}. \nYou won the round!`;
+  } else if (human === computer) {
+    result.textContent = `Both have selected ${human}! \nIt's a draw.`;
   } else {
-    playRound(humanSelection, computerSelection);
+    computerScore += 1;
+    result.textContent = `${computer} beats ${human}. \nYou lost the round!`;
   }
-} */
 
-/*   //Announce the winner of the game based on the scores. As the game is supposed to be maximum 5 rounds, a draw is possible.
-  if (humanScore > computerScore) {
-    console.log('%c You won the game! :D', `color: green;`);
-  } else if (humanScore === computerScore) {
-    console.log("%c It's a draw!", `color: orange;`);
-  } else {
-    console.log('%c You lost this time. Try again!', `color: red;`);
-  }
+  updateScore();
 
-  //Reset the variables, so you can start the game again.
- 
-
-  //Checks if player wants to start the game again.
-  if (confirm('Do you want to play again?')) {
-    reset();
-    playGame();
-  } else {
-    console.log('Thank you for playing!');
+  if (humanScore >= 5) {
+    win.play();
+    gameResult.textContent = "You've won the game :)";
+    document.getElementById('game-btns').style.pointerEvents = 'none'; //disables buttons on win/lose screen
+    newGame();
     return;
+  } else if (computerScore >= 5) {
+    gameResult.textContent = `You unfortunately lost :(\nBetter luck next time!`;
+    document.getElementById('game-btns').style.pointerEvents = 'none'; //disables buttons on win/lose screen
+    newGame();
+    return;
+  } else {
+    round++;
+
+    roundNumber.textContent = `Round: ${round}`;
+    gameResult.appendChild(result);
   }
-} */
+}
 
-/* const playAgain = document.getElementById('play-again');
+const btns = document.getElementById('game-btns');
 
-playAgain.addEventListener('mouseup', () => {
-  
-}) */
+btns.addEventListener('click', (e) => {
+  let text = e.target.closest('button').getAttribute('id');
+  if (text === 'rock' || text === 'paper' || text === 'scissors') {
+    playRound(text, getComputerChoice());
+  }
+});
+
+document.body.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON' || e.target.tagName === 'IMG') {
+    effect.currentTime = 0;
+    effect.play();
+  }
+});
